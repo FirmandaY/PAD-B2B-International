a7540670c258960b4ab4c1100b155cc443269e38
FirmandaY <firmandayahya01@mail.ugm.ac.id>
2022-04-15 11:18:47 +0700

    Upload PAD2

---
+++ app/src/main/java/com/example/b2binternational/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.b2binternational;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.b2binternational.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.chat:
+                    replaceFragment(new ChatFragment());
+                    break;
+                //case R.id.history:
+                //    replaceFragment(new HistoryFragment());
+                //    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+
+            }
+
+            return true;
+
+        });
+    }
+
+    private void  replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/com/example/b2binternational/SplashScreenActivity.java
@@ -0,0 +1,34 @@
+package com.example.b2binternational;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    //Photo by <a href="https://unsplash.com/@eiskonen?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Hans Eiskonen</a> on <a href="https://unsplash.com/s/photos/logo-design?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    //
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //Menghilangkan Status bar
+        getSupportActionBar().hide();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        int mTimeSplashScreen = 3000; //3 detik
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                SplashScreenActivity.this.startActivity(intent);
+                SplashScreenActivity.this.finish();
+            }
+        }, mTimeSplashScreen);
+    }
+}
